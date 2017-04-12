25449a52b4aec8d61d093138d0d4e16d60a04505
zhuwei <zhuwei_leo@163.com>
2017-04-12 17:21:19 +0800

    添加注册对话框

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,21 +3,34 @@
     <div class="wrapper">
       <span class="logo">Resumer</span>
       <div class="actions">
-        <button class="primary">保存</button><button>预览</button>
+        <a href="#" class="button primary" @click.prevent="signUpDialogVisible=true">注册</a>
+        <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible=false">
+          我是slote内容
+        </MyDialog>
+        <a href="#" class="button">登录</a>
       </div>
     </div>
   </div>
 </template>
 <script>
+  import MyDialog from "./MyDialog.vue"
   export default {
-      name:'topbar'
+      name:'topbar',
+      data(){
+          return{
+              signUpDialogVisible:false
+          }
+      },
+      components:{
+          MyDialog
+      }
   }
 </script>
 <style scoped lang="scss">
   #topbar{
     background: #fff;
     box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-    .wrapper{
+    >.wrapper{
       height:64px;
       padding-left:16px;
       padding-right:16px;
@@ -31,13 +44,18 @@
         color:#000;
       }
   }
-  button{
+  .button{
     width:72px;
     height:32px;
     border:none;
     font-size:18px;
     background: #ddd;
     color:#222;
+    text-decoration:none;
+    display:inline-flex;
+    justify-content:center;
+    align-items:center;
+    /*vertical-align: middle;*/
     &:hover{
       box-shadow:1px 1px 1px 1px rgba(0,0,0,.5)
     }
